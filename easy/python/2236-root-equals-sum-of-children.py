a78d9b770af7ce59db9d00bda9a8b251857f7605
Aimable Mardel <73969781+aimardel@users.noreply.github.com>
2023-05-21 17:02:28 +0100

    Challenge 2236

---
+++ easy/python/2236-root-equals-sum-of-children.py
@@ -0,0 +1,33 @@
+"""
+Challenge Description:
+You are given the root of a binary tree that consists of exactly 3 nodes: the root, its left child, and its right child.
+
+Return true if the value of the root is equal to the sum of the values of its two children, or false otherwise.
+
+Input: root = [10,4,6]
+Output: true
+Explanation: The values of the root, its left child, and its right child are 10, 4, and 6, respectively.
+10 is equal to 4 + 6, so we return true.
+"""
+# Solution:
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def checkTree(self, root):
+        if root.val == root.left + root.right:
+            return True
+        else:
+            return False
+
+
+root = TreeNode(5, 3, 2)
+solution = Solution()
+
+print(solution.checkTree(root))
